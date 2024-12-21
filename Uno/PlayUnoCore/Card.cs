1766ae4ff53294537a9e9624bcc2d5659a760d82
Vanessa Guil <vanessa.guil@etu.mines-ales.fr>
2024-12-21 19:58:26 +0100

    Debut implémentation uno core

---
+++ Uno/PlayUnoCore/Card.cs
@@ -0,0 +1,74 @@
+﻿using System.Drawing;
+using static PlayUnoCore.ENUMS;
+
+namespace PlayUnoCore
+{
+    /// <summary>
+    /// Card of uno
+    /// </summary>
+    public class Card
+    {
+        #region Enum
+        #endregion
+
+        #region Constants
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Field et Properties
+        private CardType _type;
+        public CardType Type { get { return _type; } }
+
+        private CardColor _color;
+        public CardColor Color { get { return _color; } }
+
+        private int _value;
+        public int Value { get { return _value; } } 
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="color"></param>
+        /// <param name="value"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public Card(CardType type, CardColor color, int value = -1)
+        {
+            _type = type;
+            _color = color;
+            _value = value;
+
+            // Validations: Only Number cards should have a value
+            if (type == CardType.Number && (value < 0 || value > 9))
+                throw new ArgumentException("The value of a Number card must be between 0 and 9");
+            if (type != CardType.Number && value != -1)
+                throw new ArgumentException("Only Number cards can have a value");
+        }
+        #endregion
+
+        #region Methods
+
+        #region Private and Protected Methods
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// To String
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return _type == CardType.Number
+                ? $"{_color} {_value}"
+                : $"{_color} {_type}";
+        }
+        #endregion
+
+        #endregion
+
+    }
+}
---
+++ Uno/PlayUnoCore/Game.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayUnoCore
+{
+    public class Game
+    {
+        #region Enum
+        #endregion
+
+        #region Constants
+        private const int NUMBER_OF_CARDS = 7;
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Field et Properties
+        private List<Player> _players;
+        private Deck _deck;
+        private Card _currentCard;
+        private int _currentPlayerIndex;
+        #endregion
+
+        #region Constructor
+        public Game(IEnumerable<string> playerNames)
+        {
+            if (playerNames == null || !playerNames.Any())
+                throw new ArgumentException("Il faut au moins 2 joueurs pour commencer la partie");
+
+            _players = playerNames.Select(name => new Player(name)).ToList();
+            _deck = new Deck();
+            _currentPlayerIndex = 0;
+        }
+        #endregion
+
+        #region Methods
+
+        #region Private and Protected Methods
+        private void DealInitialCards()
+        {
+            foreach (var player in _players)
+            {
+                for (int i = 0; i < NUMBER_OF_CARDS; i++)
+                {
+                    player.AddCardToHand(_deck.DrawCard());
+                }
+            }
+        }
+
+        private void PlayTurn()
+        {
+            var currentPlayer = _players[_currentPlayerIndex];
+
+            // Afficher les cartes du joueur
+
+            // Demander une carte au joueur
+
+            // Valider si la carte peut être jouée
+
+            // Pioche ?
+
+            // Passer au joueur suivant
+            _currentPlayerIndex = (_currentPlayerIndex + 1) % _players.Count;
+        }
+
+        private string GetCardsOfCurrentPlayer()
+        {
+            var currentPlayer = _players[_currentPlayerIndex];
+            var cardsInHand = currentPlayer.CardsInHand;
+
+            var cardList = cardsInHand
+                .Select((card, index) => $"{index}: {card}")
+                .ToList();
+
+            return string.Join(", ", cardList);
+        }
+
+        private Card GetChoiceOfCurrentPlayer(int indexOfCards)
+        {
+            var currentPlayer = _players[_currentPlayerIndex];
+
+            if (indexOfCards >= 0 && indexOfCards < currentPlayer.CardsInHand.Count)
+            {
+                return currentPlayer.CardsInHand[indexOfCards];
+            }
+            else
+            {
+                throw new Exception("Choix de carte non valide pour " + currentPlayer.Name);
+            }
+        }
+        #endregion
+
+        #region Public Methods
+        public bool IsGameOver()
+        {
+            return _players.Any(player => player.CardsInHand.Count == 0);
+        }
+
+        public void StartGame() 
+        {
+            DealInitialCards();
+        }
+        #endregion
+        #endregion
+    }
+}
