7625dc8f149c0390844bc06bf0ac7c861324f7f0
Vanessa Guil <vanessa.guil@etu.mines-ales.fr>
2025-01-27 11:58:25 +0100

    Ajout fichiers data

---
+++ Uno/PlayUnoData/UnoData/Card.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+
+namespace PlayUnoData.UnoData
+{
+    public class Card
+    {
+        #region Enum
+        public enum CardType
+        {
+            Number,
+            Reverse,
+            DrawTwo,
+            Skip,
+            Wild,
+            WildDrawFour
+        }
+        #endregion
+
+        #region Constants
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Field et Properties
+        private CardType _type;
+        /// <summary>
+        /// Get the type of the card
+        /// </summary>
+        public CardType Type { 
+            get { return _type; } 
+            set {  _type = value; }
+        }
+
+        private Color _color;
+        /// <summary>
+        /// Get the color of the card
+        /// </summary>
+        public Color Color { 
+            get { return _color; } 
+            set { _color = value; }
+        }
+
+        private int _value;
+        /// <summary>
+        /// Get the value of the card
+        /// </summary>
+        public int Value { 
+            get { return _value; } 
+            set { _value = value; }
+        }
+
+        /// <summary>
+        /// Get the stirng value of the card
+        /// </summary>
+        public string StringValue { get { return CreateStringValue(); } }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="color"></param>
+        /// <param name="value"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public Card(CardType type, Color color, int value = -1)
+        {
+            _type = type;
+            _color = color;
+            _value = value;
+
+            if (type == CardType.Number && (value < 0 || value > 9))
+                Logger.Instance.Log(Logger.ELevelMessage.Error, "CARD : The value of a Number card must be between 0 and 9");
+            if (type != CardType.Number && value != -1)
+                Logger.Instance.Log(Logger.ELevelMessage.Error, "CARD : Only Number cards can have a value");
+        }
+        #endregion
+
+        #region Methods
+
+        #region Private and Protected Methods
+        /// <summary>
+        /// Create the string value (for display)
+        /// </summary>
+        /// <returns></returns>
+        private string CreateStringValue()
+        {
+            if (_value != -1)
+            {
+                return _value.ToString();
+            }
+            else
+            {
+                return _type.ToString();
+            }
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// To String
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return _type == CardType.Number
+                ? $"{_color} {_value}"
+                : $"{_color} {_type}";
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Uno/PlayUnoData/UnoData/Deck.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using static PlayUnoData.UnoData.Card;
+
+namespace PlayUnoData.UnoData
+{
+    public class Deck
+    {
+        #region Enum
+        #endregion
+
+        #region Constants
+        private const int NUMBER_OF_NUMBER_CARDS = 10;
+        private const int WILD_CART_COUNT = 4; // Wild, WildDrawFour
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Field et Properties
+        private Stack<Card> _cards;
+        private Card? _currentCard;
+        private readonly Stack<Card> _oldCards;
+
+        /// <summary>
+        /// Get the current card of the deck
+        /// </summary>
+        public Card? CurrentCard { get { return _currentCard; } }
+
+        /// <summary>
+        /// Get the number of cards remaining
+        /// </summary>
+        public int RemainingCards => _cards.Count;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Deck()
+        {
+            _cards = new Stack<Card>();
+            _oldCards = new Stack<Card>();
+            InitializeDeck();
+            Shuffle();
+            _currentCard = DrawCard();
+        }
+        #endregion
+
+        #region Methods
+
+        #region Private and Protected Methods
+        /// <summary>
+        /// Initialize Deck
+        /// </summary>
+        private void InitializeDeck()
+        {
+            List<Color> colors = new List<Color> { Color.Red, Color.Blue, Color.Green, Color.Yellow };
+            // Add number cards (0-9)
+            foreach (Color color in colors)
+            {
+                // Add one 0 card
+                _cards.Push(new Card(CardType.Number, color, 0));
+
+                // Add two copies of cards 1-9
+                for (int value = 1; value < NUMBER_OF_NUMBER_CARDS; value++)
+                {
+                    _cards.Push(new Card(CardType.Number, color, value));
+                    _cards.Push(new Card(CardType.Number, color, value));
+                }
+
+                // Add two Reverse, Skip, and DrawTwo cards per color
+                _cards.Push(new Card(CardType.Reverse, color));
+                _cards.Push(new Card(CardType.Reverse, color));
+                _cards.Push(new Card(CardType.Skip, color));
+                _cards.Push(new Card(CardType.Skip, color));
+                _cards.Push(new Card(CardType.DrawTwo, color));
+                _cards.Push(new Card(CardType.DrawTwo, color));
+            }
+
+            // Add Wild and WildDrawFour cards
+            for (int i = 0; i < WILD_CART_COUNT; i++)
+            {
+                _cards.Push(new Card(CardType.Wild, Color.Black));
+                _cards.Push(new Card(CardType.WildDrawFour, Color.Black));
+            }
+        }
+
+        /// <summary>
+        /// Suffle cards
+        /// </summary>
+        private void Shuffle()
+        {
+            var cardsArray = _cards.ToArray();
+            var random = new Random();
+            for (int i = cardsArray.Length - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                (cardsArray[i], cardsArray[j]) = (cardsArray[j], cardsArray[i]);
+            }
+            _cards = new Stack<Card>(cardsArray);
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Draw a card
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public Card? DrawCard()
+        {
+            if (_cards.Count == 0)
+            {
+                if (_oldCards.Count == 0) { 
+                    Logger.Instance.Log(Logger.ELevelMessage.Error, "Problème de gestion de la pioche (vide)");
+                    return null;
+                }
+
+                // Reshuffle old cards into the deck
+                ResetDeck();
+            }
+            return _cards.Pop();
+        }
+
+        /// <summary>
+        /// Play a card
+        /// </summary>
+        /// <param name="card"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public bool PlayCard(Card card)
+        {
+            if (card == null) { 
+                Logger.Instance.Log(Logger.ELevelMessage.Error, "DECK : " + nameof(card) + " Carte ne peut pas être null");
+                return false;
+            }
+            if (_currentCard == null)
+            {
+                Logger.Instance.Log(Logger.ELevelMessage.Error, "DECK : " +nameof(_currentCard) + " Carte ne peut pas être null");
+                return false;
+            }
+
+            if (card.Color != _currentCard.Color && card.Type != _currentCard.Type && card.Color != Color.Black)
+            {
+                return false;
+            }
+
+            _oldCards.Push(_currentCard);
+            _currentCard = card;
+            return true;
+        }
+
+        /// <summary>
+        /// Reset Deck
+        /// </summary>
+        /// <param name="cards"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public void ResetDeck()
+        {
+            _cards = _oldCards;
+            _oldCards.Clear();
+            Shuffle();
+        }
+
+        /// <summary>
+        /// Restart Deck
+        /// </summary>
+        public void RestartDeck()
+        {
+            _cards.Clear();
+            _oldCards.Clear();
+            InitializeDeck();
+            Shuffle();
+            _currentCard = DrawCard();
+        }
+        #endregion
+        #endregion
+    }
+}
