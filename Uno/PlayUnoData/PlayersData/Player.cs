485e4edbe155c12e7c697c60d74fb8763d63cd82
Vanessa Guil <vanessa.guil@etu.mines-ales.fr>
2025-01-27 17:22:21 +0100

    Premier affichage cartes

---
+++ Uno/PlayUnoData/PlayersData/Player.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using PlayUnoData.UnoData;
+
+namespace PlayUnoData.PlayersData
+{
+    public class Player
+    {
+        #region Enum
+        #endregion
+
+        #region Constants
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Field et Properties
+        private string _name;
+        /// <summary>
+        /// Name of the player
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private List<Card> _cardsInHand;
+        /// <summary>
+        /// Get cards in hand
+        /// </summary>
+        public List<Card> CardsInHand
+        {
+            get { return _cardsInHand; }
+            set { _cardsInHand = value; }
+        }
+        #endregion
+
+        #region Constructor
+        public Player(string name)
+        {
+            _name = name;
+            _cardsInHand = new List<Card>();
+        }
+        #endregion
+
+        #region Methods
+
+        #region Private and Protected Methods
+        #endregion
+
+        #region Public Methods
+        public void AddCard(Card card) 
+        {
+            _cardsInHand.Add(card);
+        }
+
+        public void RemoveCard(Card card) 
+        {
+            if (_cardsInHand.Contains(card)) 
+            { 
+                _cardsInHand.Remove(card);
+            }
+            else
+            {
+                Logger.Instance.Log(Logger.ELevelMessage.Error, "PLAYER : Impossible de supprimer une carte que le joueur n'a pas");
+            }
+
+        }
+        #endregion
+
+        #endregion
+
+    }
+}
---
+++ Uno/PlayUnoGUI/ViewModel/ViewModelPlayerWindow.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using PlayUnoData.PlayersData;
+using PlayUnoData.UnoData;
+
+namespace PlayUnoGUI.ViewModel
+{
+    public class ViewModelPlayerWindow : ViewModelBase
+    {
+        #region Enum
+        #endregion
+
+        #region Constants
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Field et Properties
+        public Player _player;
+        public Deck _deck;
+
+        /// <summary>
+        /// Get the player name (for title)
+        /// </summary>
+        public string PlayerName
+        {
+            get { return _player.Name; }
+            private set
+            {
+                _player.Name = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Get Deck Card Count
+        /// </summary>
+        public int DeckCardCount
+        {
+            get { return _deck.RemainingCards; }
+        }
+
+        public int PlayerCardCount
+        {
+            get { return PlayerHand.CardsInHand.Count; }
+        }
+
+        public ViewModelCard _currentCard;
+        /// <summary>
+        /// Get the current card
+        /// </summary>
+        public ViewModelCard CurrentCard 
+        { 
+            get { return _currentCard; }
+            set
+            {
+                _currentCard = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private ViewModelCardsInHand _playerHand;
+
+        public ViewModelCardsInHand PlayerHand
+        {
+            get { return _playerHand; }
+            set 
+            { 
+                _playerHand = value; 
+                RaisePropertyChanged(); 
+            }
+        }
+
+        public int PlayerCount 
+        {
+            get { return OtherPlayers.Count + 1; } 
+        }
+
+        public List<Player> OtherPlayers;
+
+        public bool IsPlayerTurn;
+        #endregion
+
+        #region Constructor
+        public ViewModelPlayerWindow()
+        {
+            OtherPlayers = [];
+
+            _player = new Player("Pepita");
+            _deck = new Deck();
+            CurrentCard = new ViewModelCard();
+
+            PlayerHand = new ViewModelCardsInHand();
+            for (int i = 0; i < 7; i++)
+            {
+                PlayerHand.AddCard(_deck.DrawCard());
+            }
+
+            if (_deck.CurrentCard != null)
+                CurrentCard.CardObject = _deck.CurrentCard;
+            
+        }
+        #endregion
+
+        #region Methods
+
+        #region Private and Protected Methods
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #endregion
+
+    }
+}
